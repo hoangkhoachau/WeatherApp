30db3818eeceeae68bb2d34de505f7914029ebf3
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-11 19:35:02 +0300

    Create LocationManager

---
+++ Sources/Manager/LocationManager.swift
@@ -0,0 +1,51 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 11/07/2024.
+//
+
+import CoreLocation
+import Foundation
+
+class LocationManager: NSObject, ObservableObject {
+    
+    private let manager = CLLocationManager()
+    
+    @Published var userLocation: CLLocation?
+    
+    static let shared = LocationManager ()
+    override init() {
+        super.init()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+    }
+    
+    func checkLocationAuthorization() {
+        switch manager.authorizationStatus{
+            
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            
+        case .authorizedWhenInUse, .authorizedAlways:
+            manager.startUpdatingLocation()
+            self.userLocation = manager.location
+            
+        default:
+            break
+            
+        }
+    }
+    
+    func isLocationServicesDenied() -> Bool {
+        manager.authorizationStatus == .denied
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
