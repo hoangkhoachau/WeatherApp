9e0704793fcd7a2138259b0db817b4b97e814f62
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-11 19:52:32 +0300

    Create City Model

---
+++ Sources/Models/City.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 11/07/2024.
+//
+
+import Foundation
+
+struct City: Codable {
+    var name: String
+    let lat: Double
+    let lon: Double
+}
