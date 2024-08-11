e2e296d7c03a86359662e0daa6ff3beaf231fa45
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-11 17:34:43 +0300

    Setup Xcode Files

---
+++ Sources/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 11/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sources/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
