0877c7cedaccf2cf0060ad2d4ca6a9fc9ead7afb
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-12 22:49:35 +0300

    Create a color extension to get color foreach weather type

---
+++ Sources/Extensions/Color+Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Color+Extensions.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 12/07/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static func forecastBackground(weatherId: Int?) -> Color {
+        
+        guard let weatherId else { return .green }
+        
+        switch weatherId {
+        case 200...399:
+            return .orange
+            
+        case 300...499:
+            return .gray
+            
+        case 500...599:
+            return .indigo
+            
+        case 600...699:
+            return .cyan
+            
+        case 700...799:
+            return .brown
+            
+        case 800:
+            return .blue
+            
+        case 801...899:
+            return .gray
+            
+        default:
+            return .green
+        }
+    }
+    
+}
