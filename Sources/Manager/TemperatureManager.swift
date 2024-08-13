e7b7a4294248e653b149756d1995fbc75be7a03a
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-14 02:47:38 +0300

    Create temperature manager
    - To access saved preference
    - To convert temperature degree

---
+++ Sources/Manager/TemperatureManager.swift
@@ -0,0 +1,25 @@
+//
+//  TemperatureManager.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 13/07/2024.
+//
+
+import SwiftUI
+
+enum DegreeType: String {
+    case celsius, fahrenheit
+}
+
+@MainActor final class TemperatureManager: ObservableObject {
+    @AppStorage("DegreeType") var degreeType: DegreeType = .celsius
+    
+    func getTemperature(_ temperature: Double) -> Int {
+        switch degreeType {
+        case .celsius:
+            return Int(temperature)
+        case .fahrenheit:
+            return Int((temperature * (9/5)) + 32)
+        }
+    }
+}
