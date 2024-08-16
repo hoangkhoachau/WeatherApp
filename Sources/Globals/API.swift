58fb04353ee1f4c2722025180c0b9a9109820092
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-16 17:58:03 +0300

    Create API.swift

---
+++ Sources/Globals/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  Weather
+//
+//  Created by Chau Hoang Khoa on 11/07/2024.
+//
+
+import Foundation
+
+struct API {
+    #warning("Add your API key")
+    static let key = ""
+}
